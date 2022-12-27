8b183cf04c278bb3f71552fea750fdb18f5f04da
Abdulloh <albybarnes17@gmail.com>
2022-12-27 13:09:08 +0600

    hw7

--- main.py
+++ main.py
@@ -1,62 +1,29 @@
-from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
 import sys
+from pytube import YouTube
 
 
 class Form(QMainWindow):
     def __init__(self):
         super(Form, self).__init__()
 
-        loadUi('calc.ui', self)
-        self.add.clicked.connect(self.add_num)
-        self.min.clicked.connect(self.min_num)
-        self.mul.clicked.connect(self.mul_num)
-        self.div.clicked.connect(self.div_num)
+        loadUi('ytdownload.ui', self)
 
-    def add_num(self):
-        try:
-            num1 = int(self.input1.text())
-            num2 = int(self.input2.text())
-            self.res.setText(str(num1 + num2))
-        except:
-            self.res.setText('Input numbers!')
+        self.audio.clicked.connect(self.audio_download)
+        self.video.clicked.connect(self.video_download)
 
-    
-    def min_num(self):
-        try:
-            num1 = int(self.input1.text())
-            num2 = int(self.input2.text())
-            self.res.setText(str(num1 - num2))
-        except:
-            self.res.setText('Input numbers!')
+    def audio_download(self):
+        url = str(self.url1.text())
+        yout = YouTube(url)
+        yout.streams.filter(only_audio=True).first().download("audio", f"{yout.title}.mp3")
+        self.maintitle.text("Скачано!")
 
-
-
-    def mul_num(self):
-        try:
-            num1 = int(self.input1.text())
-            num2 = int(self.input2.text())
-            self.res.setText(str(num1 * num2))
-        except:
-            self.res.setText('Input numbers!')
-
-    
-    def div_num(self):
-        try:
-            try:
-                num1 = int(self.input1.text())
-                num2 = int(self.input2.text())
-                self.res.setText(str(num1 / num2))
-            except:
-                if num1 == 0 and num2 == 0:
-                    self.res.setText('Dumb! Division by zero is not allowed!')
-                else:
-                    pass
-        except:
-            self.res.setText('Input numbers!')
-
-    
+    def video_download(self):
+        url = str(self.url1.text())
+        yout = YouTube(url)
+        yout.streams.filter(progressive=True, file_extension="mp4").first().download("video", f"{yout.title}.mp4")
+        self.maintitle.text("Скачано!")
 
 app = QApplication(sys.argv)
 form = Form()
